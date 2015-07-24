00922b41bfa356d133b539c013853419c82ad92f
Gary Kang <dragon_gary@msn.com>
2015-07-24 18:24:31 +0000

    comments schema del projectID

--- db/comments.js
+++ db/comments.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var commentsSchema = new Schema({
-	"ProjectId": {type: Schema.ObjectId, ref: 'Project'},
 	"UserId": {type: Schema.ObjectId, ref: 'User'},
 	"Time": {type: Date},
 	"Content": {type: String}
