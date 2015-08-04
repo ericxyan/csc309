8b447f40513198a1aea072a1a59b37b890243ee5
lbg940131 <alice_ut@163.com>
2015-08-03 23:33:34 -0400

    commit

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var routes = require('./routes/index');
-var users = require('./routes/users');
 var api = require('./routes/api');
 var passport = require('passport');
 var session = require('express-session');
@@ -44,7 +42,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', routes);
-app.use('/users', users);
 app.use('/api', api);
 app.use('/auth', authenticate);
 
